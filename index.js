1eb5580671f78cfc2f34c116195ed7348558e074
gavriguy <gavri.guy@gmail.com>
2020-01-27 17:05:09 +0200

    fix feed

--- index.js
+++ index.js
@@ -15,45 +15,45 @@ export default {
     testCommand: "gavri-dsp://fetchData?type=search&q=private%20ryan"
   },
   handler: providerInterface => async params => {
-    console.log({params});
-    
+    console.log({ params });
+
     const appData = await providerInterface.appData();
     console.log(appData);
 
     return await providerInterface.sendResponse({
       type: {
         value: "feed",
-        title: "title",
-        entry: [
-          {
-            type: {
-              value: "video"
-            },
-            id: "Id1",
-            title: "Example Video Title #1",
-            summary: "Example summary",
-            published: "2005-04-06T13:00:00-08:00",
-            updated: "2005-04-06T20:25:05-08:00",
-            media_group: [
-              {
-                type: "image",
-                media_item: [
-                  {
-                    type: "image",
-                    key: "image_base",
-                    src: "https://via.placeholder.com/320x180.png"
-                  }
-                ]
-              }
-            ],
-            content: {
-              type: "video/hls",
-              src:
-                "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"
+        title: "title"
+      },
+      entry: [
+        {
+          type: {
+            value: "video"
+          },
+          id: "Id1",
+          title: "Example Video Title #1",
+          summary: "Example summary",
+          published: "2005-04-06T13:00:00-08:00",
+          updated: "2005-04-06T20:25:05-08:00",
+          media_group: [
+            {
+              type: "image",
+              media_item: [
+                {
+                  type: "image",
+                  key: "image_base",
+                  src: "https://via.placeholder.com/320x180.png"
+                }
+              ]
             }
+          ],
+          content: {
+            type: "video/hls",
+            src:
+              "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"
           }
-        ]
-      }
+        }
+      ]
     });
   }
 };
