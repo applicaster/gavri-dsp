cfd2a2b62d64ab2c901e724134f5dd6bb2ea45ea
gavriguy <gavri.guy@gmail.com>
2020-01-28 16:02:39 +0200

    use one file - for some reason I cannot export

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
-import screen from "./feeds/screen";
-import component1 from "./feeds/component1";
+
+function screen({params}) {
+  return {
+    type: {
+      value: "feed",
+      title: "title"
+    },
+    entry: [
+      {
+        type: {
+          value: "feed"
+        },
+        content: {
+          src: `gavri-dsp://fetchData?type=search&q=${params.q}&component=1`
+        }
+      }
+    ]
+  };
+}
+
+function component1({ params }) {
+  return {
+    type: {
+      value: "feed",
+      title: "title"
+    },
+    entry: [
+      {
+        type: {
+          value: "video"
+        },
+        id: "Id1",
+        title: "Example Video Title #1",
+        summary: "Example summary",
+        published: "2005-04-06T13:00:00-08:00",
+        updated: "2005-04-06T20:25:05-08:00",
+        media_group: [
+          {
+            type: "image",
+            media_item: [
+              {
+                type: "image",
+                key: "image_base",
+                src: "https://via.placeholder.com/320x180.png"
+              }
+            ]
+          }
+        ],
+        content: {
+          type: "video/hls",
+          src: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"
+        }
+      }
+    ]
+  };
+}
 
 export default {
   name: "gavri-dsp",
